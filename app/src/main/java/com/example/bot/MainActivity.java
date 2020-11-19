2853333dbe40aab42fb42d6ee89a236b9f8fe343
Ritika Saini <ritikasaini123456@gmail.com>
2020-11-19 16:08:02 +0530

    chatbot using dialogflow

---
+++ app/src/main/java/com/example/bot/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.bot;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bot.adapters.ChatAdapter;
+import com.example.bot.helpers.SendMessageInBg;
+import com.example.bot.interfaces.BotReply;
+import com.example.bot.models.Message;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.dialogflow.v2.DetectIntentResponse;
+import com.google.cloud.dialogflow.v2.QueryInput;
+import com.google.cloud.dialogflow.v2.SessionName;
+import com.google.cloud.dialogflow.v2.SessionsClient;
+import com.google.cloud.dialogflow.v2.SessionsSettings;
+import com.google.cloud.dialogflow.v2.TextInput;
+import com.google.common.collect.Lists;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+import static com.example.bot.R.*;
+
+public class MainActivity extends AppCompatActivity implements BotReply {
+
+    RecyclerView chatView;
+    ChatAdapter chatAdapter;
+    List<Message> messageList = new ArrayList<>();
+    EditText editMessage;
+    ImageButton btnSend;
+
+    //dialogFlow
+    private SessionsClient sessionsClient;
+    private SessionName sessionName;
+    private String uuid = UUID.randomUUID().toString();
+    private String TAG = "mainactivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+        chatView = findViewById(id.chatView);
+        editMessage = findViewById(id.editMessage);
+        btnSend = findViewById(id.btnSend);
+
+        chatAdapter = new ChatAdapter(messageList, this);
+        chatView.setAdapter(chatAdapter);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override public void onClick(View view) {
+                String message = editMessage.getText().toString();
+                if (!message.isEmpty()) {
+                    messageList.add(new Message(message, false));
+                    editMessage.setText("");
+                    sendMessageToBot(message);
+                    Objects.requireNonNull(chatView.getAdapter()).notifyDataSetChanged();
+                    Objects.requireNonNull(chatView.getLayoutManager())
+                            .scrollToPosition(messageList.size() - 1);
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter text!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        setUpBot();
+    }
+
+    private void setUpBot() {
+        try {
+            InputStream stream = this.getResources().openRawResource(raw.credential);
+            GoogleCredentials credentials = GoogleCredentials.fromStream(stream)
+                    .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+            String projectId = ((ServiceAccountCredentials) credentials).getProjectId();
+
+            SessionsSettings.Builder settingsBuilder = SessionsSettings.newBuilder();
+            SessionsSettings sessionsSettings = settingsBuilder.setCredentialsProvider(
+                    FixedCredentialsProvider.create(credentials)).build();
+            sessionsClient = SessionsClient.create(sessionsSettings);
+            sessionName = SessionName.of(projectId, uuid);
+
+            Log.d(TAG, "projectId : " + projectId);
+        } catch (Exception e) {
+            Log.d(TAG, "setUpBot: " + e.getMessage());
+        }
+    }
+
+    private void sendMessageToBot(String message) {
+        QueryInput input = QueryInput.newBuilder()
+                .setText(TextInput.newBuilder().setText(message).setLanguageCode("en-US")).build();
+        new SendMessageInBg(this, sessionName, sessionsClient, input).execute();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void callback(DetectIntentResponse returnResponse) {
+        if(returnResponse!=null) {
+            String botReply = returnResponse.getQueryResult().getFulfillmentText();
+            if(!botReply.isEmpty()){
+                messageList.add(new Message(botReply, true));
+                chatAdapter.notifyDataSetChanged();
+                Objects.requireNonNull(chatView.getLayoutManager()).scrollToPosition(messageList.size() - 1);
+            }else {
+                Toast.makeText(this, "something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "failed to connect!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
